df2f0a074a73d7179113f2e4adb34602726cb7e4
LOGESHWARIM <47376868+LOGESHWARIM@users.noreply.github.com>
2019-02-06 01:02:13 -0800

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num;
+		Scanner s1=new Scanner(System.in);
+		num=s1.nextInt();
+		if(num%2==0)
+		System.out.println("Even");
+		else if(num%2>0)
+		System.out.println("Odd");
+		else
+		System.out.println("invalid");
+	}
+}
